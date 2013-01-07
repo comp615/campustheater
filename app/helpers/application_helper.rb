7f2f614765a6c55b0eb5f50b7d286f61a9ef8b16
Charlie Croom <charlie.croom@gmail.com>
2013-01-06 17:43:35 -0800

    Dammit again Jared!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -80,9 +80,10 @@ module ApplicationHelper
 	def square_show_thumb(poster, lazy = false)
 	    c = (poster.height(:thumb) > poster.width(:thumb)) ? "poster-vertical" : "poster-horizontal"
 	    if lazy
-	        "<img src=\"/assets/placeholder.gif\" data-original=\"#{poster.url(:thumb)}\" class=\"#{c} lazy\">"
+	      "<img src=\"/assets/placeholder.gif\" data-original=\"#{poster.url(:thumb)}\" class=\"#{c} lazy\">"
 	    else
-	        "<img src=\"#{poster.url(:thumb)}\" class=\"#{c}\">"
+	      "<img src=\"#{poster.url(:thumb)}\" class=\"#{c}\">"
+	    end
 	end
 	
 	def link_to_show_title(show)
